d3f2396d81e849296c0afed06923184f77b782c9
Bowen Sun <bowensun@gmail.com>
2017-08-29 19:01:01 +0100

    * dirty bash scripts, fix later

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,17 @@ ENV WHITELIST null
 ENV SG_ID null
 ENV AWS_ACCESS_KEY_ID null
 ENV AWS_SECRET_ACCESS_KEY null
+ENV AWS_DEFAULT_REGION null
 
 RUN echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/02apt-speedup && \
     apt-get update && \
-    apt-get install -y curl jq ipcalc && \
+    apt-get install -y cron jq && \
     pip install awscli && \
     rm -rf /var/lib/apt/lists/*
 
 COPY crontab /etc/cron.d/openshift-blocker
-RUN chmod 0644 /etc/cron.d/openshift-blocker
-RUN touch /var/log/cron.log
+COPY run.sh /run.sh
+RUN chmod 0644 /etc/cron.d/openshift-blocker && touch /var/log/cron.log && chmod +x /run.sh
 
+USER root:root
 CMD cron && tail -f /var/log/cron.log
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+X_SG_ID=($SG_ID)
+X_WHITELIST=($WHITELIST)
+
+for SG in $SG_ID; do
+  aws ec2 revoke-security-group-egress --group-id $SG --ip-permissions "$(aws ec2 describe-security-groups --group-ids $SG | jq '.SecurityGroups[].IpPermissionsEgress')"
+done
+
+declare -i COUNT=0
+declare -i MULTIPLIER=0
+for I in `seq 0 $(expr ${#X_WHITELIST[@]} - 1)`; do
+  if [[ $COUNT -lt 25 ]]; then
+    echo $I - ${X_WHITELIST[$I]} - ${X_SG_ID[$MULTIPLIER]}
+    if [[ ${X_WHITELIST[$I]} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}$ ]]; then
+      aws ec2 authorize-security-group-egress --group-id ${X_SG_ID[$MULTIPLIER]} --ip-permissions '{"IpProtocol": "tcp", "FromPort": 80, "ToPort": 80, "IpRanges": [{"CidrIp": "'${X_WHITELIST[$I]}'"}]}'
+      aws ec2 authorize-security-group-egress --group-id ${X_SG_ID[$MULTIPLIER]} --ip-permissions '{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443, "IpRanges": [{"CidrIp": "'${X_WHITELIST[$I]}'"}]}'
+    else
+      aws ec2 authorize-security-group-egress --group-id ${X_SG_ID[$MULTIPLIER]} --ip-permissions '{"IpProtocol": "tcp", "FromPort": 80, "ToPort": 80, "IpRanges": [{"CidrIp": "'${X_WHITELIST[$I]}'/32"}]}'
+      aws ec2 authorize-security-group-egress --group-id ${X_SG_ID[$MULTIPLIER]} --ip-permissions '{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443, "IpRanges": [{"CidrIp": "'${X_WHITELIST[$I]}'/32"}]}'
+    fi
+    COUNT+=1
+  else
+    echo "Warning: Security Group ${X_SG_ID[$MULTIPLIER]} reach maximum 50 egress rules."
+    MULTIPLIER+=1
+    COUNT=0
+  fi
+done
